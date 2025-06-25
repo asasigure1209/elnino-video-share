c0fe5aa3c2c8ea73909e903ac5eacb17a5628e57
asasigure <mkmokymh@gmail.com>
2025-06-25 22:45:03 +0900

    biomeのformat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'エルニーニョ動画配布サイト',
-  description: 'ヲタ芸バトルイベント「エルニーニョ」の動画配布サイト',
+  title: "エルニーニョ動画配布サイト",
+  description: "ヲタ芸バトルイベント「エルニーニョ」の動画配布サイト",
 }
 
 export default function RootLayout({
