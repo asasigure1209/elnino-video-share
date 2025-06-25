f0ca26d810055b90a52ebf4fb38d937fc435ddd8
asasigure <mkmokymh@gmail.com>
2025-06-25 22:32:30 +0900

    Next.jsのセットアップ

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'エルニーニョ動画配布サイト',
+  description: 'ヲタ芸バトルイベント「エルニーニョ」の動画配布サイト',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
