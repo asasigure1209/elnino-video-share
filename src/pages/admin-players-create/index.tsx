59404ea1bc6721325567a4ac9eddb4b122cfb47a
asasigure <mkmokymh@gmail.com>
2025-07-05 02:35:39 +0900

    build失敗する要因のファイルを一旦削除

--- src/pages/admin-players-create/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CreatePlayerForm } from "../../features/manage-players/components/create-player-form"
-
-export function AdminPlayersCreatePage() {
-  return (
-    <div className="w-full">
-      <CreatePlayerForm />
-    </div>
-  )
-}
